80ecba310db91516634397a2f0b8691fdc870dfd
ahmed-abdelhafiz <ahmedabdelhafiz1@gmail.com>
2023-08-29 18:04:42 +0300

    feat(formatDateTime): add formatDateTime helper function

--- src/composables/UseGeneralHelpers.ts
+++ src/composables/UseGeneralHelpers.ts
@@ -1,14 +1,40 @@
 export default function UseGeneralHelpers() {
   /***************************************
- **** Section Functions Declaration ****
- **************************************/
+   **** Section Variables Declaration ****
+   **************************************/
+  // #region Variables
+  const { locale } = useI18n()
+
+  // #endregion
+
+  /***************************************
+   **** Section Functions Declaration ****
+   **************************************/
   // #region Functions
   function getImageUrl(path: string): any {
     return new URL(`../assets/images/${path}`, import.meta.url).href
   }
 
+  /**
+   * Format date time to be in the format of: dd/mm/yyyy hh:mm
+   * @param {string}  date date to be formatted
+   * @returns {string}  formatted date
+   */
+  function formatDateTime(date: string): string {
+    const language = locale.value === 'ar' ? 'ar-EG' : 'en-US'
+
+    return new Date(date).toLocaleDateString(language, {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    })
+  }
+
   // #endregion
   return {
     getImageUrl,
+    formatDateTime,
   }
 }
