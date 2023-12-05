95f4499d2b501be00a27c9a40f45578c6cb39c05
Ahmed <ahmedabdelhafiz1@gmail.com>
2023-12-05 21:00:39 +0200

    feat(logs page): remove filter icon

--- src/pages/LogsPage.vue
+++ src/pages/LogsPage.vue
@@ -3,22 +3,16 @@ import UseGeneralHelpers from '@/composables/UseGeneralHelpers'
 import { UseNotificationsHelpers } from '@/composables/UseNotificationsHelpers'
 import { UseCrudHelpers } from '@/composables/UserCrudHelpers'
 import type { DashboardNotification } from '@/interfaces/Notifications'
-import type { pageAction } from '@/interfaces/Shared'
 import { logsService } from '@/services/LogsService'
 
 /***************************************
  **** Section Variables Declaration ****
  **************************************/
 // #region Variables
-const FilterComponent = defineAsyncComponent(
-  () => import('@/components/notifications/NotificationsFilter.vue'),
-)
 const { t } = useI18n()
 const { formatDateTime } = UseGeneralHelpers()
 const { handleNotificationAction } = UseNotificationsHelpers()
 const MODEL_NAME = 'logs'
-const showFilter = ref<boolean>(false)
-const loadFilter = ref<boolean>(false)
 
 const params: any = reactive({
   page: 1,
@@ -58,60 +52,19 @@ const headers: any = [
 // #endregion
 
 /***************************************
- **** Section Computed Variables  ******
- **************************************/
-// #region Computed
-const pageActionsButtons = computed<pageAction[]>(() => {
-  return [
-    {
-      icon: 'tabler-filter',
-      show: true,
-      handler: handleShowFilter,
-    },
-  ]
-})
-
-// #endregion
-
-/***************************************
  **** Section Lifecycle Hooks  *********
  **************************************/
 // #region Lifecycle Hooks
 getPageData()
 
 // #endregion
-
-/***************************************
- **** Section Functions Declaration ****
- **************************************/
-// #region Functions
-function handleShowFilter() {
-  // TODO show filter
-  // showFilter.value = !showFilter.value
-  // if (!loadFilter.value) loadFilter.value = true
-}
-
-function onApplyFilter(filters: any) {
-  Object.assign(params, { ...filters, page: 1 })
-  getPageData()
-}
-
-// #endregion
 </script>
 
 <template>
   <section>
-    <Component
-      :is="FilterComponent"
-      v-if="loadFilter"
-      v-model:showFilter="showFilter"
-      @apply-filter="onApplyFilter"
-      :init-filters="params"
-    />
     <VCard title="سجل العمليات" class="page-card">
       <VCardText>
         <PageActions
-          :page-actions-buttons="pageActionsButtons"
           :items-per-page="params.itemPerPage"
           :model="MODEL_NAME"
           @update:items-per-page="onChangeItemsPerPage"
